62e47d00e93d8cefeb67f3ca60b94d30157714d7
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-03-29 20:31:17 +0530

    added static files and template to main.html

--- GroupMeetPlanner/settings.py
+++ GroupMeetPlanner/settings.py
@@ -122,6 +122,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

0fb45677666350bd8fa9b3c629ffd6e82b99d307
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-04-02 21:26:03 +0530

    updated the User model and added some improvements

--- base/models.py
+++ base/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(null=True)
-    bio = models.TextField(null=True)
+    email = models.EmailField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
     # default_time = models.ForeignKey(
     #     'Time', on_delete=models.SET_NULL, null=True, blank=True, related_name='default_time'
     # )
